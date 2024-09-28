dc924661878d8d05717fd4b3d9637b3953a1f589
Mark Beale <bealebeaverst@gmail.com>
2024-09-27 20:08:05 -0700

    working react side

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import { UserContext } from "../contexts/UserContext";
+// import "./NavBar.css";
 
 function NavBar() {
+  const { user, logout } = useContext(UserContext);
+
   return (
     <nav className="navbar">
-    <ul>
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/bus-stops">Bus Stops</NavLink></li>
-      {user ? (
-        <>
-          <li><NavLink to="/favorites">Favorites</NavLink></li>
-          <li><NavLink to="/profile">Profile</NavLink></li>
-          <li><button onClick={logout}>Logout</button></li>
-        </>
-      ) : (
-        <li><NavLink to="/login">Login</NavLink></li>
-      )}
-    </ul>
-  </nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/bus-stops">Bus Stops</NavLink>
+        </li>
+        {user ? (
+          <>
+            <li>
+              <NavLink to="/favorites">Favorites</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">Profile</NavLink>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 }
 export default NavBar;
