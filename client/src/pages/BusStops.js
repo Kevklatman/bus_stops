203f5b7feda5ffaee90f331b237c2dbe32b651b1
Mark Beale <bealebeaverst@gmail.com>
2024-09-28 14:28:43 -0700

    correct port set

--- client/src/pages/BusStops.js
+++ client/src/pages/BusStops.js
@@ -7,7 +7,7 @@ function BusStops() {
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5555";
 
   useEffect(() => {
     setIsLoading(true);
