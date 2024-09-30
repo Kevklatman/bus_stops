2a33e42a7e5d40b4b47e50e074098f1bee967656
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-09-30 14:17:02 -0400

    updated the favorite list

--- client/src/components/FavoriteList.js
+++ client/src/components/FavoriteList.js
@@ -5,27 +5,27 @@ import { Link } from "react-router-dom";
 function FavoriteList({ favorites }) {
   
   const [search, setSearch] = useState("");
- 
   const updateSearch = (newSearch) => setSearch(newSearch);
-
+  
+  const filteredFavorites = favorites.filter((favorite) => favorite.bus_stop_name.toLowerCase().includes(search.toLowerCase())); 
 
   
   return (
     <div className="favorite-list">
-      {favorites.map((favorite) => (
+      {filteredFavorites.map((favorite) => (
         <div key={favorite.id} className="favorite-card">
           <h3>{favorite.bus_stop_name}</h3>
           <p>{favorite.bus_stop_location}</p>
           <Link to={`/schedule/${favorite.bus_stop_id}`}>View Schedule</Link>
 
-        <label htmlFor="search">Favrotie Bus Stops</label>
+        <label htmlFor="search">Favorite Bus Stops</label>
         <input
-        value={search}
-        type="text"
-        id="search"
-        placeholder="Search bus stops..."
-        onChange={(e) => updateSearch(e.target.value)}
-      />
+          value={search}
+          type="text"
+          id="search"
+          placeholder="Search bus stops..."
+          onChange={(e) => updateSearch(e.target.value)}
+        />
         </div>
       ))}
     </div>
